5a911dd005b9be56da13bbc2749a7ac26bb42535
Ireneusz Szulc <ireneusz.szulc@deepsense.ai>
2022-08-11 17:25:34 +0200

    Squash the commits history

---
+++ README.md
@@ -0,0 +1,41 @@
+# Racetrack Plugin: Teams Notifications
+
+This plugin sends Racetrack notifications to Teams channel.
+
+It sends events when:
+- job is submitted (fatman deployed)
+- fatman is deleted
+
+## Setup
+1. Create webhook for a Teams channel - 
+  https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook
+2. Activate the plugin in Racetrack, 
+  add the following to your Lifecycle configuration (in kustomize ConfigMap):
+
+```yaml
+plugins:
+- name: teams-notifier
+  git_remote: https://github.com/TheRacetrack/plugin-teams-notifications
+  git_ref: '1.0.0'
+  git_directory: teams-notifier
+```
+
+3. Set `TEAMS_WEBHOOK` environment variable to your webhook URL for Lifcycle component.
+  You can set different webhooks for different deployment environments.
+  For instance:
+  ```yaml
+  apiVersion: apps/v1
+  kind: Deployment
+  metadata:
+    namespace: racetrack
+    name: lifecycle
+  spec:
+    replicas: 1
+    template:
+      spec:
+        containers:
+          - name: lifecycle
+            env:
+              - name: TEAMS_WEBHOOK
+                value: 'https://webhook.office.com/webhookb2/...'
+```
---
+++ teams-notifier/plugin.py
@@ -0,0 +1,43 @@
+import os
+from typing import Optional
+from pathlib import Path
+
+import pymsteams
+
+from racetrack_client.manifest import Manifest
+from racetrack_commons.entities.dto import FatmanDto
+from racetrack_commons.plugin.core import PluginCore
+
+
+class Plugin(PluginCore):
+
+    def __init__(self) -> None:
+        self.webhook_url = os.environ.get('TEAMS_WEBHOOK')
+        assert self.webhook_url, 'TEAMS_WEBHOOK env var was not set'
+
+    def post_fatman_deploy(self, manifest: Manifest, fatman: FatmanDto, image_name: str, deployer_username: str = None):
+        """Supplementary actions invoked after fatman is deployed"""
+        cluster = os.environ.get('CLUSTER_FQDN')
+        self._send_notification(f'Fatman {fatman} has been deployed to {cluster} cluster by {deployer_username}')
+
+    def post_fatman_delete(self, fatman: FatmanDto, username_executor: str = None):
+        """Supplementary actions invoked after fatman is deleted"""
+        cluster = os.environ.get('CLUSTER_FQDN')
+        self._send_notification(f'Fatman {fatman} has been deleted from {cluster} cluster by {username_executor}')
+
+    def markdown_docs(self) -> Optional[str]:
+        """
+        Return documentation for this plugin in markdown format
+        """
+        readme_file: Path = self.plugin_dir.parent / 'README.md'
+        readme_markdown = readme_file.read_text()
+        info_markdown = f"""
+# Plugin configuration
+- **Plugin version (git ref)**: {self.plugin_config.git_ref}
+        """
+        return readme_markdown + info_markdown
+
+    def _send_notification(self, message: str):
+        notification = pymsteams.connectorcard(self.webhook_url)
+        notification.text(message)
+        notification.send()
